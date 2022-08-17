8f4e88acaa4ffad1c3cf713c7b751ea5b7f720aa
Viktor Marinho <viktormpcs@gmail.com>
2022-08-17 08:06:18 -0300

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::{thread_rng, Rng};
 use std::io;
 
 fn input(message: &str) -> String {
@@ -13,9 +14,28 @@ fn input(message: &str) -> String {
 }
 
 fn main() {
-    let number = input("Digite um número:")
-        .parse::<i32>()
-        .expect("Erro: não foi digitado um número");
+    let random_number = thread_rng().gen_range(1..=100);
+    let mut tentativas = 0;
+    println!("Tente adivinhar o número que sorteei entre 1 e 100!");
 
-    println!("{number}");
+    let correct_guess = loop {
+        tentativas += 1;
+        let guess = input("Digite um número:")
+            .parse::<i32>()
+            .expect("Erro: não foi digitado um número");
+
+        if guess > random_number {
+            println!("Muito alto!");
+        } else if guess < random_number {
+            println!("Muito baixo!");
+        } else {
+            println!("Parabéns! você acertou!");
+            break guess;
+        }
+    };
+
+    println!(
+        "A resposta correta era {}. Você precisou de {} tentativas.",
+        correct_guess, tentativas
+    );
 }
